de80391157b7cb10a0c63553593af35e73aeaa0d
mkotyak <kotyak.maria@gmail.com>
2022-07-28 15:32:43 +0300

    Lecture 6 Demo

--- Memorize/Screens/Game/CardView.swift
+++ Memorize/Screens/Game/CardView.swift
@@ -10,6 +10,10 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    PieShape(
+                        startAngle: Angle(degrees: 0-90),
+                        endAngle: Angle(degrees: 110-90)
+                    ).padding(5).opacity(0.5)
                     Text(card.content)
                         .font(font(in: geometry.size))
                 } else {
@@ -33,6 +37,6 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
+        static let fontScale: CGFloat = 0.7
     }
 }
